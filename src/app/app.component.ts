ee1cc3bb6c9c4541bfaaed8f6909f4cc8687eac8
Ezgi Keserci <keserci.ezgi@gmail.com>
2024-08-17 01:13:39 +0300

    Update app.component.ts and app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'northwind';
+  // typescript'te tip güvenliği sağlamak için degisken_adi:type olarak yazılır.
+  title: string = 'Northwind';
+
+  user: string = 'Ezgi Keserci';
+
+  // any tipi ile her türlü veriyi tutabiliriz.
+  product1: any = {
+    productId: 1,
+    productName: 'Dondurma',
+    categoryId: 2,
+    unitPrice: 15
+  };
+
+  products = [
+    { productId: 32432, productName: 'Dondurma', categoryId: 2, unitPrice: 15 },
+    { productId: 11129, productName: 'Çikolata', categoryId: 2, unitPrice: 20 },
+    { productId: 24535, productName: 'Çilek', categoryId: 2, unitPrice: 25 },
+    { productId: 67874, productName: 'Muz', categoryId: 2, unitPrice: 30 },
+    { productId: 50032, productName: 'Elma', categoryId: 2, unitPrice: 35 }
+  ];
+
+  user1 = { userId: 1, userName: 'Ezgi' };
+  user2 = { userId: 2, userName: 'Elif' };
+  user3 = { userId: 3, userName: 'Ece' };
+
+  users = [this.user1, this.user2, this.user3];
 }
